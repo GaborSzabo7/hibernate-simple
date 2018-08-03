25d04b08bcd160b81aaf14bd19d15cdd2c4f655f
gaszabo7 <gaszabo7@gmail.com>
2018-08-03 23:33:40 +0200

    introduce Log4j2 with SLF4J

--- src/main/java/hu/gaszabo/hibernate/simple/Application.java
+++ src/main/java/hu/gaszabo/hibernate/simple/Application.java
@@ -1,9 +1,14 @@
 package hu.gaszabo.hibernate.simple;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Application {
 
+	private static Logger LOGGER = LoggerFactory.getLogger(Application.class);
+	
 	public static void main(String[] args) {
-
+		LOGGER.warn("Application start...");
 	}
 
 }
